255e5c7bf43e5f37a537d16b0a0b1939753122d3
gunil0610 <gunil.na1@gmail.com>
2023-05-17 12:19:34 +0900

    feat: reusable button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import Button from "@/components/ui/Button";
+
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
                 <p className="text-red-500">hello</p>
+                <Button>Button</Button>
             </div>
         </main>
     );
